4ecdaa2f54c9637258d5b39d64d4106f55728809
Khang <nldkhang@gmail.com>
2026-07-14 20:06:35 +0700

    chore: change cookie sameSite to none

--- be/src/routes/auth.js
+++ be/src/routes/auth.js
@@ -70,13 +70,13 @@ router.post("/login", async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 ngày
     });
     res.cookie("role", role, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 ngày
     });
 
