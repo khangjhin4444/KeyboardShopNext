67bf9b921f254f605328ed6e20b5cb0920093e6b
Khang <nldkhang@gmail.com>
2026-07-14 19:11:27 +0700

    feat: implement get order and cancel order api for admin

--- be/src/controllers/order.controller.js
+++ be/src/controllers/order.controller.js
@@ -75,4 +75,77 @@ const cancelOrder = async (req, res) => {
     return res.status(500).json({ success: false, message: "Lỗi máy chủ." });
   }
 };
-module.exports = { getOrders, cancelOrder };
+
+const getAdminOrders = async (req, res) => {
+  try {
+    const status = req.query.status;
+    const orders = await sql`
+      SELECT 
+        o."OrderID",
+        o."UserID",
+        o."Date",
+        o."Shipping",
+        o."Status",
+        o."Payment",
+        o."Name" AS "ReceiverName", 
+        o."Phone",
+        o."Address",
+        o."Total",
+        o."Request",
+        json_agg(
+          json_build_object(
+            'OrderItemID', oi."OrderItemID",
+            'Name', p."Name",
+            'Color', pv."Color",
+            'MainImage', pv."MainImage",
+            'Quantity', oi."Quantity",
+            'Price', oi."Price" 
+          )
+        ) AS items
+        FROM "order" o
+        JOIN "order_items" oi ON o."OrderID" = oi."OrderID"
+        JOIN "product_variants" pv ON oi."VariantID" = pv."VariantID"
+        JOIN "product" p ON pv."ProductID" = p."ProductID"
+        WHERE o."Status" = ${status}
+        GROUP BY o."OrderID"
+        ORDER BY o."Date" DESC;`;
+    return res.status(200).json({
+      success: true,
+      length: orders ? orders.length : 0,
+      data: orders,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const cancelAdminOrder = async (req, res) => {
+  try {
+    const { orderID } = req.body; // Hoặc req.body tùy cách bạn thiết kế route
+
+    const result = await sql`
+      UPDATE "order"
+      SET "Status" = 'Canceled'
+      WHERE "OrderID" = ${orderID} 
+        AND "Status" = 'Pending'
+      RETURNING "OrderID";
+    `;
+    console.log(result);
+    if (result.rowCount === 0) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Không thể hủy đơn hàng này. Đơn hàng không tồn tại hoặc đã vượt qua giai đoạn chờ xử lý.",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Đã hủy đơn hàng thành công!",
+    });
+  } catch (error) {
+    console.error("Lỗi khi hủy đơn hàng:", error);
+    return res.status(500).json({ success: false, message: "Lỗi máy chủ." });
+  }
+};
+module.exports = { getOrders, cancelOrder, getAdminOrders, cancelAdminOrder };
--- be/src/routes/order.route.js
+++ be/src/routes/order.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middlewares/verifyToken");
+const verifyAdmin = require("../middlewares/verifyAdmin");
 const orderController = require("../controllers/order.controller");
 
 router.use(verifyToken);
@@ -8,5 +9,9 @@ router.use(verifyToken);
 router.get("/", orderController.getOrders);
 
 router.put("/cancel", orderController.cancelOrder);
+
+router.get("/admin", verifyAdmin, orderController.getAdminOrders);
+
+router.put("/admin/cancel", verifyAdmin, orderController.cancelAdminOrder);
 
 module.exports = router;
