b00ba080f53d2449e9c13608eecb551fe3325ec8
Khang <nldkhang@gmail.com>
2026-07-21 21:25:37 +0700

    chore: remove unused import

--- fe/src/app/(private)/(no header)/admin/products/page.tsx
+++ fe/src/app/(private)/(no header)/admin/products/page.tsx
@@ -1,40 +1,5 @@
 "use client";
-import { useState } from "react";
-import {
-  useInfiniteQuery,
-  useMutation,
-  useQuery,
-  useQueryClient,
-} from "@tanstack/react-query";
 
-import { Button } from "@/components/ui/button";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Badge } from "@/components/ui/badge";
-import { Plus, Pencil, Trash2 } from "lucide-react";
-import { toast } from "sonner";
-
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
-import { AdminService } from "@/features/admin/services/admin.service";
-import { AdminUsecase } from "@/features/admin/usecase/admin.usecase";
-import { DataTable } from "../products/_component/data-table";
-import { columns } from "../products/_component/column";
-import { div } from "motion/react-m";
 import ProductTable from "./_component/product-table";
 
 export default function Page() {
--- fe/src/app/(public)/layout.tsx
+++ fe/src/app/(public)/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { div } from "framer-motion/client";
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
